e126c7a7f5d4594149dc55762387d2c736359ae1
Nithin Reddy <nithin.reddy@glu.com>
2020-04-20 19:35:34 +0530

    Updates

---
+++ precache-manifest.d04da7a05918d3aed018a5a600138a16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2eb898f214dd6ac7546659a119218d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6eecbdde164b8befe5",
+    "url": "/static/css/main.19f1033b.chunk.css"
+  },
+  {
+    "revision": "c11ee6acffe993168bc7",
+    "url": "/static/js/2.c20faf65.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.c20faf65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6eecbdde164b8befe5",
+    "url": "/static/js/main.dd6e30bb.chunk.js"
+  },
+  {
+    "revision": "bcb3e891f8c02b6d3b30",
+    "url": "/static/js/runtime-main.2d5c5ce2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

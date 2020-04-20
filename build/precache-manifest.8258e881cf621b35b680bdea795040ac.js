0c56f4f80d5cba07584f31091300ab304485d468
Nithin Reddy <nithin.reddy@glu.com>
2020-04-20 17:05:14 +0530

    depenencies

---
+++ build/precache-manifest.8258e881cf621b35b680bdea795040ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1537a6d18ae9f50bd5ba35c882ae807",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "49a4c6e3f3b3730c7769",
+    "url": "/react-game/static/css/main.19f1033b.chunk.css"
+  },
+  {
+    "revision": "c11ee6acffe993168bc7",
+    "url": "/react-game/static/js/2.c20faf65.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/react-game/static/js/2.c20faf65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a4c6e3f3b3730c7769",
+    "url": "/react-game/static/js/main.8da4bd7d.chunk.js"
+  },
+  {
+    "revision": "233d9e73aaacb8628054",
+    "url": "/react-game/static/js/runtime-main.4005dc2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-game/static/media/logo.5d5d9eef.svg"
+  }
+]);

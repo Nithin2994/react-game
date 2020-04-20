1b85454646288422058f2a5511d87f7da79415dc
Nithin Reddy <nithin.reddy@glu.com>
2020-04-20 17:54:26 +0530

    build

--- build/precache-manifest.19dd99b28b2af49d6463ff568a91e098.js
+++ build/precache-manifest.19dd99b28b2af49d6463ff568a91e098.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ffd285f5c6f1571d8b1d29bb96f4608c",
+    "revision": "ba558c7e9c7e62c9f4ec8fc14f3abe50",
     "url": "/react-game/index.html"
   },
   {
-    "revision": "e9b5b30769c2d4665c93",
+    "revision": "708a2a0846b0512fdda2",
     "url": "/react-game/static/css/main.19f1033b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-game/static/js/2.c20faf65.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e9b5b30769c2d4665c93",
-    "url": "/react-game/static/js/main.fd7bf111.chunk.js"
+    "revision": "708a2a0846b0512fdda2",
+    "url": "/react-game/static/js/main.6a968a93.chunk.js"
   },
   {
     "revision": "233d9e73aaacb8628054",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-game/precache-manifest.e669b04453f5a0f4697a794652724cd1.js"
+  "/react-game/precache-manifest.19dd99b28b2af49d6463ff568a91e098.js"
 );
 
 self.addEventListener('message', (event) => {

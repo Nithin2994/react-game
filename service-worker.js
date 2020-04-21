4eda672bc33a7157f7a1e7d84218a97999f057eb
Nithin Reddy <nithin.reddy@glu.com>
2020-04-21 19:08:58 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c640269338328f7fdac1644c29f0fa8.js"
+  "/precache-manifest.0294cf48ae37836a3906527c35af7eda.js"
 );
 
 self.addEventListener('message', (event) => {
